d5dcc237525c8a8e2de3e71fd1f8ab357baa479f
Jhostin Luna <jhostinlunadev@gmail.com>
2024-05-26 20:34:27 +0200

    implement show error on ListCharacterFragment and DetailCharacterFragment when it receive an Network Connection Error

--- app/src/main/java/com/jhostinluna/heroes/core/platform/NetworkHandler.kt
+++ app/src/main/java/com/jhostinluna/heroes/core/platform/NetworkHandler.kt
@@ -3,7 +3,6 @@ package com.jhostinluna.heroes.core.platform
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
